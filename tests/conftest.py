437e94f29f0237b503459187276b15f815e36e72
Kristina Maximova <maximovaki@gmail.com>
2025-02-03 18:34:10 +0300

    add test for Product & Category

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from src.categories import Category
 
 
 @pytest.fixture
-def test_product():
+def test_product_1():
     return Product(
         name="некий товар",
         description="некий товар удивит вас",
@@ -14,17 +14,47 @@ def test_product():
     )
 
 
+def test_product_2():
+    return Product(
+        name="некий другой товар",
+        description="другой товар озадачит вас",
+        price=1.12,
+        quantity=5
+    )
+
+
 @pytest.fixture
-def test_category():
+def test_category_1():
     return Category(
         name="некая категория",
         description="Это расширит ваши возможности",
-        products=[Product_1("некий товар",
-                            "некий товар удивит вас",
-                            9.11,
-                            3),
-                  Product_2("некий другой товар",
-                            "другой товар озадачит вас",
-                            1.12,
-                            5)]
+        products=[Product("некий товар",
+                          "некий товар удивит вас",
+                          9.11,
+                          3),
+                  Product("некий другой товар",
+                          "другой товар озадачит вас",
+                          1.12,
+                          5)]
+    )
+
+
+@pytest.fixture
+def test_category_2():
+    return Category(
+        name="иная категория",
+        description="Это вдохновит вас",
+        products=[Product("иной товар",
+                          "иной товар не удивит вас",
+                          0.11,
+                          2),
+                  Product("другой иной товар",
+                          "другой иной товар не озадачит вас",
+                          0.12,
+                          6),
+                  Product("совсем иной товар",
+                          "иной товар не затруднит вас",
+                          11.2,
+                          8)]
+
     )
---
+++ tests/test_categories.py
@@ -0,0 +1,14 @@
+from src.categories import Category
+from src.products import Product
+
+
+def test_category_init(test_category_1, test_category_2):
+    assert test_category_1.name == "некая категория"
+    assert test_category_1.description == "Это расширит ваши возможности"
+    assert len(test_category_1.products) == 2
+
+    assert test_category_1.category_count == 2
+    assert test_category_2.category_count == 2
+
+    assert test_category_1.product_count == 5
+    assert test_category_2.product_count == 5
